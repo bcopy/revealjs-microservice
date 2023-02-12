d7cc1ee7acbcc1f87bfd61424e54e2aa97e99778
bcopy <500789+bcopy@users.noreply.github.com>
2023-02-12 13:26:14 +0100

    Add support for local path override

--- src/main/java/com/github/cmcrobotics/reveal/RevealJsMicroserviceApplication.java
+++ src/main/java/com/github/cmcrobotics/reveal/RevealJsMicroserviceApplication.java
@@ -1,12 +1,14 @@
 package com.github.cmcrobotics.reveal;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @SpringBootApplication
 public class RevealJsMicroserviceApplication {
@@ -18,11 +20,23 @@ public class RevealJsMicroserviceApplication {
 	@Configuration
 	@EnableWebMvc
 	public class MvcConfig implements WebMvcConfigurer {
+		@Value("${static.path:./www/}")
+		private String staticPath;
+
 		@Override
 		public void addResourceHandlers(ResourceHandlerRegistry registry) {
 			registry
-			.addResourceHandler("/revealjs/**")
-			.addResourceLocations("/revealjs/");	
+					.addResourceHandler("revealjs/**")
+					.addResourceLocations("classpath:/revealjs/");
+			System.out.println(" ********************\n************ static path "+staticPath);
+			registry
+					.addResourceHandler("/**")
+					.addResourceLocations("file:" + staticPath, "classpath:/static/");
+		}
+
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addViewController("/").setViewName("/index.html");
 		}
 	}
 
